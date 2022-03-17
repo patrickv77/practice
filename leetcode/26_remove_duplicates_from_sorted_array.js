94ed80aa00797b582bae9122d241477d724f304e
patrickv77 <patrickv77@aol.com>
2022-03-17 13:22:43 -0400

    leetcode 26

---
+++ leetcode/26_remove_duplicates_from_sorted_array.js
@@ -0,0 +1,28 @@
+/*
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+*/
+
+/*
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var removeDuplicates = function(nums) {
+    for(var i  = 0; i < nums.length-1; i++) {
+        if(nums[i] == nums[i+1]) {
+            nums.splice(i+1, 1);
+            i--;
+        }
+    }
+    return nums.length;
+};
+
+/*
+Runtime: 230 ms, faster than 10.69% of JavaScript online submissions for Remove Duplicates from Sorted Array.
+Memory Usage: 44.5 MB, less than 63.85% of JavaScript online submissions for Remove Duplicates from Sorted Array.
+ */
