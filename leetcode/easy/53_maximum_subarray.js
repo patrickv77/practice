de3f59a2ca4e3b2df72fd2a74aa09a4c93f91426
patrickv77 <patrickv77@aol.com>
2022-04-12 16:32:44 -0400

    leetcode 53 solution

--- leetcode/easy/53_maximum_subarray.js
+++ leetcode/easy/53_maximum_subarray.js
@@ -9,10 +9,34 @@ A subarray is a contiguous part of an array.
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    
+    var maxSub = 0;
+    var currSub = 0;
+    var maxEle = nums[0];
+
+    for(var i = 0; i < nums.length; i++){
+        currSub += nums[i];
+        if(currSub < 0) {
+            currSub = 0;
+        }
+
+        if(currSub > maxSub) {
+            maxSub = currSub;
+        }
+
+        if(nums[i] > maxEle) {
+            maxEle = nums[i];
+        }
+    }
+
+    //if maxSub = 0, all elements are less than or equal to 0 which means the max subarray is equal to the largest element
+    if(maxSub === 0) {
+        return maxEle;
+    }
+
+    return maxSub;
 };
 
 /*
-
-
+Runtime: 126 ms, faster than 31.16% of JavaScript online submissions for Maximum Subarray.
+Memory Usage: 50.2 MB, less than 82.18% of JavaScript online submissions for Maximum Subarray.
 */
