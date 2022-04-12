dea47af5ffcb4986857ce257ecfafc9aab5445cd
patrickv77 <patrickv77@aol.com>
2022-04-12 16:14:22 -0400

    leetcode 53 setup

---
+++ leetcode/easy/53_maximum_subarray.js
@@ -0,0 +1,18 @@
+/*
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+A subarray is a contiguous part of an array.
+*/
+
+/*
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    
+};
+
+/*
+
+
+*/
