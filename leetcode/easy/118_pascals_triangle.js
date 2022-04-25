ccab02a2144a78768f95ab4e9760899e78570ca9
patrickv77 <patrickv77@aol.com>
2022-04-25 14:43:18 -0400

    leetcode 118 solution

--- leetcode/easy/118_pascals_triangle.js
+++ leetcode/easy/118_pascals_triangle.js
@@ -8,10 +8,39 @@ In Pascal's triangle, each number is the sum of the two numbers directly above i
  * @param {number} numRows
  * @return {number[][]}
  */
- var generate = function(numRows) {
+var generate = function(numRows) {  
+    var res = [[1]];
     
+    if(numRows === 1) {
+        return res;
+    }else if(numRows > 30) {
+        return "no";
+    }else{
+        for(var i = 1; i < numRows; i++){
+            res.push(pascal(res[i-1], i+1));
+        }
+    }
+
+    return res;
+};
+
+var pascal = function(arr, len){
+    var tempArr = new Array(len);
+    if(len === 2) {
+        tempArr[0] = 1;
+        tempArr[1] = 1;
+    }else{
+        tempArr[0] = 1;
+        for(var i = 1; i < tempArr.length-1; i++){
+            tempArr[i] = arr[i-1] + arr[i];
+        }
+        tempArr[tempArr.length-1] = 1;
+    }
+
+    return tempArr;
 };
 
 /*
-
+Runtime: 56 ms, faster than 95.33% of JavaScript online submissions for Pascal's Triangle.
+Memory Usage: 42.3 MB, less than 36.62% of JavaScript online submissions for Pascal's Triangle.
  */
