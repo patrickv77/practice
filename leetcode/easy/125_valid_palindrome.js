3a272c2c41cf8e2a841f7e40923b61d21d111f21
patrickv77 <patrickv77@aol.com>
2022-04-25 15:55:55 -0400

    leetcode 125 solution

--- leetcode/easy/125_valid_palindrome.js
+++ leetcode/easy/125_valid_palindrome.js
@@ -7,10 +7,26 @@ Given a string s, return true if it is a palindrome, or false otherwise.
  * @param {string} s
  * @return {boolean}
  */
- var isPalindrome = function(s) {
-    
+var isPalindrome = function(s) {
+    var regex = /[^a-z0-9]/g;
+
+    s = s.toLowerCase();
+    s = s.replace(regex, "");
+
+    var itrForward = 0;
+    var itrBackward = s.length-1;
+
+    for(itrForward; itrForward < parseInt(s.length/2); itrForward++) {
+        if(s.charAt(itrForward) != s.charAt(itrBackward)){
+            return false;
+        }
+        itrBackward--;
+    }
+
+    return true;
 };
 
 /*
-
+Runtime: 127 ms, faster than 21.19% of JavaScript online submissions for Valid Palindrome.
+Memory Usage: 44.4 MB, less than 80.33% of JavaScript online submissions for Valid Palindrome.
  */
--- leetcode/leetcode_playground.js
+++ leetcode/leetcode_playground.js
@@ -9,35 +9,41 @@
 // b%=2;
 // console.log(b);
 
-var addBinary = function(a, b) {
+// var addBinary = function(a, b) {
 
-    if(a.length > b.length) {
-        b = b.padStart(a.length, "0");
-    }else{
-        a = a.padStart(b.length, "0");
-    }
+//     if(a.length > b.length) {
+//         b = b.padStart(a.length, "0");
+//     }else{
+//         a = a.padStart(b.length, "0");
+//     }
 
-    var res = "";
-    var carry = 0;
+//     var res = "";
+//     var carry = 0;
 
-    for(var i = a.length-1; i >= 0; i--) {
-        var temp = parseInt(a.charAt(i)) + parseInt(b.charAt(i)) + carry;
-        if(temp > 1) {
-            carry = 1;
-            temp %= 2;
-            res = temp.toString() + res;
-        }else{
-            carry = 0;
-            res = temp.toString() + res;
-        }
-    }
-    if(carry == 1) {
-        res = "1" + res;
-    }
+//     for(var i = a.length-1; i >= 0; i--) {
+//         var temp = parseInt(a.charAt(i)) + parseInt(b.charAt(i)) + carry;
+//         if(temp > 1) {
+//             carry = 1;
+//             temp %= 2;
+//             res = temp.toString() + res;
+//         }else{
+//             carry = 0;
+//             res = temp.toString() + res;
+//         }
+//     }
+//     if(carry == 1) {
+//         res = "1" + res;
+//     }
 
-    return res;
-};
+//     return res;
+// };
 
-console.log(addBinary("1010", "1011"));
+// console.log(addBinary("1010", "1011"));
 
 //expected: "10101"
+
+var a = "ABCECBA";
+
+for(var i = 0; i < parseInt(a.length/2); i++ ){
+    console.log(a.charAt(i));
+}
