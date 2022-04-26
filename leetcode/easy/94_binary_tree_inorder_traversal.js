53c37b1bbc2325becb84ab7cb31338c11a3b46bc
patrickv77 <patrickv77@aol.com>
2022-04-26 18:05:17 -0400

    leetcode 94 solution

--- leetcode/easy/94_binary_tree_inorder_traversal.js
+++ leetcode/easy/94_binary_tree_inorder_traversal.js
@@ -15,10 +15,23 @@ Given the root of a binary tree, return the inorder traversal of its nodes' valu
  * @param {TreeNode} root
  * @return {number[]}
  */
- var inorderTraversal = function(root) {
+var inorderTraversal = function(root) {
+    const res = [];
+
+    traverse = function(node) {
+        if(!node) return;
+
+        traverse(node.left);
+        res.push(node.val);
+        traverse(node.right);
+    }
     
+    traverse(root);
+    
+    return res;
 };
 
 /*
-
+Runtime: 69 ms, faster than 66.70% of JavaScript online submissions for Binary Tree Inorder Traversal.
+Memory Usage: 42.2 MB, less than 52.17% of JavaScript online submissions for Binary Tree Inorder Traversal.
 */
