ad5cb84620fad9ba8941c69d656f0ec116d7ce0b
patrickv77 <patrickv77@aol.com>
2022-05-16 14:59:11 -0500

    leetcode 258 solution

--- leetcode/easy/258_add_digits.js
+++ leetcode/easy/258_add_digits.js
@@ -7,9 +7,16 @@ Given an integer num, repeatedly add all its digits until the result has only on
  * @return {number}
  */
 var addDigits = function(num) {
-    
+    while(num > 9){
+        var temp = Array.from(num.toString()).map(Number);
+        num = 0;
+        temp.forEach(x => num += x);
+    }
+
+    return num;
 };
 
 /*
-
+Runtime: 124 ms, faster than 20.58% of JavaScript online submissions for Add Digits.
+Memory Usage: 44.5 MB, less than 15.40% of JavaScript online submissions for Add Digits.
  */
