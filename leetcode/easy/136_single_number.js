f5c3ddd98ba4cd133bc97a9fa6386a95974d1e94
patrickv77 <patrickv77@aol.com>
2022-05-16 13:46:44 -0500

    leetcode 136 solution

--- leetcode/easy/136_single_number.js
+++ leetcode/easy/136_single_number.js
@@ -9,9 +9,34 @@ You must implement a solution with a linear runtime complexity and use only cons
  * @return {number}
  */
 var singleNumber = function(nums) {
-    
+    const hm = new Map();
+
+    for(var i = 0; i < nums.length; i++) {
+        if(!hm.has(nums[i])){
+            hm.set(nums[i], 1);
+        }else{
+            hm.delete(nums[i]);
+        }
+    }
+
+    const keyItr = hm.keys();
+
+    return keyItr.next().value;
 };
 
 /*
+Runtime: 74 ms, faster than 85.37% of JavaScript online submissions for Single Number.
+Memory Usage: 47.8 MB, less than 19.38% of JavaScript online submissions for Single Number.
+*/
 
+/*
+bitwise XOR is insane in this application:
+
+res = nums[0]
+
+for(i=1; i < nums.length; i++){
+    res = res^nums[i]
+}
+
+return res;
 */
