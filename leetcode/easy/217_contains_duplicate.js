8c5df2f29674cd667038a0925fac8fade14d84fa
patrickv77 <patrickv77@aol.com>
2022-05-17 14:50:44 -0500

    leetcode 217 solution

--- leetcode/easy/217_contains_duplicate.js
+++ leetcode/easy/217_contains_duplicate.js
@@ -6,10 +6,19 @@ Given an integer array nums, return true if any value appears at least twice in 
  * @param {number[]} nums
  * @return {boolean}
  */
- var containsDuplicate = function(nums) {
-    
+var containsDuplicate = function(nums) {
+    const hm = new Map();
+    for(let i = 0; i < nums.length; i++) {
+        if(hm.has(nums[i])) {
+            return false;
+        } else { 
+            hm.set(nums[i], 1);
+        }
+    }
+    return true;
 };
 
 /*
-
+Runtime: 123 ms, faster than 50.43% of JavaScript online submissions for Contains Duplicate.
+Memory Usage: 51 MB, less than 45.76% of JavaScript online submissions for Contains Duplicate.
  */
