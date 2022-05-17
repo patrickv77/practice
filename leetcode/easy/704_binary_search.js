2842a11171b2bc528d2f0854425785174829ae1f
patrickv77 <patrickv77@aol.com>
2022-05-17 13:07:29 -0500

    leetcode 704 setup

---
+++ leetcode/easy/704_binary_search.js
@@ -0,0 +1,18 @@
+/*
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+ */
+
+/*
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    
+};
+
+/*
+
+ */
