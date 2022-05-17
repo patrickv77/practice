2fef6edf4ee90dab04ba317b0f408a94bd1d76b0
patrickv77 <patrickv77@aol.com>
2022-05-17 13:06:33 -0500

    leetcode 278 solution

--- leetcode/easy/278_first_bad_version.js
+++ leetcode/easy/278_first_bad_version.js
@@ -26,10 +26,24 @@ var solution = function(isBadVersion) {
      * @return {integer} The first bad version
      */
     return function(n) {
-        
+        let r = n;
+        let l = 0;
+        while(l <= r) {
+            let mid = Math.floor((l+r)/2);
+            let version = isBadVersion(mid);
+            if(version !== isBadVersion(mid+1)){
+                return mid+1;
+            }else if(version === false) {
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+        }
     };
 };
 
 /*
-
+Runtime: 82 ms, faster than 43.59% of JavaScript online submissions for First Bad Version.
+Memory Usage: 42.2 MB, less than 19.90% of JavaScript online submissions for First Bad Version.
  */
+
