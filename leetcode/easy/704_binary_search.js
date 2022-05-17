908075980f3b9a8cc6dbac17e43aca69469f3152
patrickv77 <patrickv77@aol.com>
2022-05-17 13:14:50 -0500

    leetcode 704 solution

--- leetcode/easy/704_binary_search.js
+++ leetcode/easy/704_binary_search.js
@@ -10,9 +10,22 @@ You must write an algorithm with O(log n) runtime complexity.
  * @return {number}
  */
 var search = function(nums, target) {
-    
+    let len = nums.length-1;
+    let l = 0, r = len;
+    while(l <= r) {
+        let mid = Math.floor((l+r)/2);
+        if(nums[mid] === target) {
+            return mid;
+        } else if(nums[mid] > target) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return -1;
 };
 
 /*
-
+Runtime: 70 ms, faster than 80.04% of JavaScript online submissions for Binary Search.
+Memory Usage: 45.2 MB, less than 34.83% of JavaScript online submissions for Binary Search.
  */
