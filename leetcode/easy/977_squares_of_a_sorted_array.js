a166c3d4b49e8ab8a4617b5cf20bda0e8e0626ec
patrickv77 <patrickv77@aol.com>
2022-05-18 11:40:01 -0500

    leetcode 977 setup

---
+++ leetcode/easy/977_squares_of_a_sorted_array.js
@@ -0,0 +1,15 @@
+/*
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+*/
+
+/*
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    
+};
+
+/*
+
+ */
