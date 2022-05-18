699d6b5fba22377d58c5cb6bb7a2da2a1bd58027
patrickv77 <patrickv77@aol.com>
2022-05-18 12:37:18 -0500

    leetcode 189 setup

---
+++ leetcode/medium/189_rotate_array.js
@@ -0,0 +1,16 @@
+/*
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+*/
+
+/*
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    
+};
+
+/*
+
+*/
