8a1e326812212445a20d967044d31641cdb83fe5
patrickv77 <patrickv77@aol.com>
2022-05-18 12:19:58 -0500

    leetcode 977 solution

--- leetcode/easy/977_squares_of_a_sorted_array.js
+++ leetcode/easy/977_squares_of_a_sorted_array.js
@@ -7,8 +7,16 @@ Given an integer array nums sorted in non-decreasing order, return an array of t
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
+    for(let i = 0; i < nums.length; i++) {
+        nums[i] *= nums[i];
+    }
     
+    return nums.sort(function(a,b){
+        return a-b;
+    });
 };
+
+console.log(sortedSquares([-4,-1,0,3,10]));
 
 /*
 
