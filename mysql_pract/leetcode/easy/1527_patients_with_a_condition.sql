ee5e346dd4cd02d58559aed65fdd24a5741a4668
patrickv77 <patrickv77@aol.com>
2022-05-19 13:16:59 -0500

    leetcode 1527 solution

--- mysql_pract/leetcode/easy/1527_patients_with_a_condition.sql
+++ mysql_pract/leetcode/easy/1527_patients_with_a_condition.sql
@@ -9,13 +9,13 @@ insert into Patients (patient_id, patient_name, conditions) values ('3', 'Bob', 
 insert into Patients (patient_id, patient_name, conditions) values ('4', 'George', 'ACNE DIAB100');
 insert into Patients (patient_id, patient_name, conditions) values ('5', 'Alain', 'DIAB201');
 
+SELECT * FROM Patients;
 
-
-
-
-
+SELECT * FROM Patients
+WHERE conditions RLIKE '(?<= )DIAB1.*|^DIAB1.*';
 
 
 /*
-
+Runtime: 338 ms, faster than 53.57% of MySQL online submissions for Patients With a Condition.
+Memory Usage: 0B, less than 100.00% of MySQL online submissions for Patients With a Condition.
 */
