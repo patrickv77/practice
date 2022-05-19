7aac7f4dc7f26dbeebecbfadcc5c1433c72d5bc6
patrickv77 <patrickv77@aol.com>
2022-05-19 11:44:00 -0500

    leetcode 1484 setup

--- mysql_pract/leetcode/easy/1667_fix_names_in_a_table.sql
+++ mysql_pract/leetcode/easy/1667_fix_names_in_a_table.sql
@@ -9,8 +9,9 @@ insert into Users (user_id, name) values ('2', 'bOB');
 SELECT * FROM Users;
 
 #UPDATE Users
-#SET name = UPPER(LEFT(name, 1)) + LOWER(SUBSTRING(name, 2, LEN(name)));
+#SET name = CONCAT(UPPER(LEFT(name, 1)) + LOWER(RIGHT(name, (LENGTH(name)-1))));
 
+DROP TABLE Users;
 
 
 SELECT user_id,
@@ -18,6 +19,7 @@ CONCAT(
 UPPER(LEFT(name,1)), LOWER(RIGHT(name,(LENGTH(name)-1)))
 )AS name
 FROM Users ORDER BY 1;
+
 
 /*
 Runtime: 1245 ms, faster than 5.01% of MySQL online submissions for Fix Names in a Table.
