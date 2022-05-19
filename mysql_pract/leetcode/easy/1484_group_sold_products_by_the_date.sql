9116613d6132303597ad1f6da04bf92edc95e768
patrickv77 <patrickv77@aol.com>
2022-05-19 12:12:40 -0500

    leetcode 1484 solution

--- mysql_pract/leetcode/easy/1484_group_sold_products_by_the_date.sql
+++ mysql_pract/leetcode/easy/1484_group_sold_products_by_the_date.sql
@@ -11,3 +11,24 @@ insert into Activities (sell_date, product) values ('2020-06-01', 'Bible');
 insert into Activities (sell_date, product) values ('2020-06-02', 'Mask');
 insert into Activities (sell_date, product) values ('2020-05-30', 'T-Shirt');
 
+SELECT * FROM Activities;
+
+SELECT sell_date, COUNT(DISTINCT product) AS num_sold, group_concat(DISTINCT product) AS products
+FROM Activities 
+GROUP BY 1
+ORDER BY 1;
+
+/*
+Runtime: 641 ms, faster than 12.47% of MySQL online submissions for Group Sold Products By The Date.
+Memory Usage: 0B, less than 100.00% of MySQL online submissions for Group Sold Products By The Date.
+*/
+
+
+/* **REFERENCE**
+SELECT b.name AS band_name, COUNT(a.id) AS num_albums
+FROM bands AS b
+LEFT JOIN albums AS a ON b.id = a.band_id
+WHERE b.name = 'Deuce'
+GROUP BY b.id
+HAVING num_albums = 1;
+*/
