082c561f2b03072f0fd24201f5ef4af4a23f86e3
patrickv77 <patrickv77@aol.com>
2022-05-20 13:19:33 -0500

    leetcode 1795 setup

---
+++ mysql_pract/leetcode/easy/1795_rearrange_products_table.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE 1795_rearrange_products_table;
+USE 1795_rearrange_products_table;
+
+Create table If Not Exists Products (product_id int, store1 int, store2 int, store3 int);
+Truncate table Products;
+insert into Products (product_id, store1, store2, store3) values ('0', '95', '100', '105');
+insert into Products (product_id, store1, store2, store3) values ('1', '70', NULL, '80');
+
+
+
+/*
+
+*/
