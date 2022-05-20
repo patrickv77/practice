d8ef556c5889f6ca41ec366bf32f2cb1f3a267d8
patrickv77 <patrickv77@aol.com>
2022-05-20 16:00:48 -0500

    leetcode 608 setup

--- mysql_pract/leetcode/easy/608_tree_node.sql
+++ mysql_pract/leetcode/easy/608_tree_node.sql
@@ -3,7 +3,7 @@ USE 608_tree_node;
 
 Create table If Not Exists Tree (id int, p_id int);
 Truncate table Tree;
-insert into Tree (id, p_id) values ('1', 'None');
+insert into Tree (id, p_id) values ('1', NULL);
 insert into Tree (id, p_id) values ('2', '1');
 insert into Tree (id, p_id) values ('3', '1');
 insert into Tree (id, p_id) values ('4', '2');
