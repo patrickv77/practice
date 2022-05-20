36321ec82d2f39a964e5cbfd585a4bb6d3a3eedf
patrickv77 <patrickv77@aol.com>
2022-05-20 16:21:29 -0500

    leetcode 577 solution

--- leetcode/easy/557_reverse_words_in_a_string_III.js
+++ leetcode/easy/557_reverse_words_in_a_string_III.js
@@ -7,9 +7,16 @@ Given a string s, reverse the order of characters in each word within a sentence
  * @return {string}
  */
 var reverseWords = function(s) {
-    
+    let t = s.split(" ");
+
+    for(let i = 0; i < t.length; i++) {
+        t[i] = t[i].split("").reverse().join("");
+    }
+
+    return t.join(" ");
 };
 
 /*
-
+Runtime: 102 ms, faster than 45.21% of JavaScript online submissions for Reverse Words in a String III.
+Memory Usage: 47.8 MB, less than 97.57% of JavaScript online submissions for Reverse Words in a String III.
 */
