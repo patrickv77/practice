986877707ac998297575b0c6706471c68a5cc9cb
patrickv77 <patrickv77@aol.com>
2022-05-20 16:16:15 -0500

    leetcode 344 solution

--- leetcode/easy/344_reverse_string.js
+++ leetcode/easy/344_reverse_string.js
@@ -9,9 +9,34 @@ You must do this by modifying the input array in-place with O(1) extra memory.
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    
+    let len = s.length;
+    let even = false;
+    if(len%2 === true) even = true;
+
+    let r = 0, l = len-1;
+    if(even) {
+        while(r < len/2) {
+            let temp = s[r];
+            s[r] = s[l];
+            s[l] = temp;
+            r++;
+            l--;
+        }
+    } else {
+        let mid = (r+l)/2;
+        while(r < mid) {
+            let temp = s[r];
+            s[r] = s[l];
+            s[l] = temp;
+            r++;
+            l--;
+        }
+    }
 };
 
-/*
+console.log(reverseString(['H','e','l','l','o']));
 
+/*
+Runtime: 102 ms, faster than 73.18% of JavaScript online submissions for Reverse String.
+Memory Usage: 49 MB, less than 85.34% of JavaScript online submissions for Reverse String.
 */
