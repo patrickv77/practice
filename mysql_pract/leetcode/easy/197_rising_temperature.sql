444f2829e4c4bbb7164c7e1d1d7e319e8e60cc3f
patrickv77 <patrickv77@aol.com>
2022-05-23 14:21:37 -0500

    leetcode 197 setup

---
+++ mysql_pract/leetcode/easy/197_rising_temperature.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE 197_rising_temperature;
+USE 197_rising_temperature;
+
+Create table If Not Exists Weather (id int, recordDate date, temperature int);
+Truncate table Weather;
+insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10');
+insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25');
+insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20');
+insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30');
+
+SELECT * FROM Weather;
