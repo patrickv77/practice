af56941228c0254c8ed2145d77cf9b9e2b9c2c12
patrickv77 <patrickv77@aol.com>
2022-05-23 11:21:23 -0500

    leetcode 608 solution

--- mysql_pract/leetcode/easy/608_tree_node.sql
+++ mysql_pract/leetcode/easy/608_tree_node.sql
@@ -9,3 +9,18 @@ insert into Tree (id, p_id) values ('3', '1');
 insert into Tree (id, p_id) values ('4', '2');
 insert into Tree (id, p_id) values ('5', '2');
 
+SELECT * FROM Tree;
+
+SELECT id,
+CASE
+	WHEN p_id IS NULL THEN 'Root'
+    WHEN id IN (SELECT p_id FROM Tree) then 'Inner'
+	ELSE 'Leaf'
+END AS type
+FROM Tree
+ORDER BY id;
+
+/*
+Runtime: 518 ms, faster than 27.88% of MySQL online submissions for Tree Node.
+Memory Usage: 0B, less than 100.00% of MySQL online submissions for Tree Node.
+*/
