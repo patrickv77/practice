b2e742bd82a5df91bdf1b0bef572e1d107361ab3
patrickv77 <patrickv77@aol.com>
2022-05-23 15:46:44 -0500

    leetcode 74 progress 5.23.22

--- leetcode/74_search_a_2d_matrix.js
+++ leetcode/74_search_a_2d_matrix.js
@@ -11,8 +11,56 @@ The first integer of each row is greater than the last integer of the previous r
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    
+    var findRow = function(matrix, target) {
+        let numRows = matrix.length;
+        let low = 0, high = numRows - 1;
+        let mid = Math.floor((high + low) / 2);
+
+        let rowFound = false;
+        while(!rowFound) {
+            
+            if(matrix[mid][0] <= target && matrix[mid][matrix[mid].length-1] >= 0) {
+                rowFound = true;
+            }
+
+            if(mid === low || mid === high) {
+                return mid;
+            }
+
+            if(matrix[mid][0] > target) {
+                high = mid-1;
+                mid = Math.floor((high + low) / 2);
+            } else if (matrix[mid][0] < target) {
+                low = mid + 1;
+                mid = Math.floor((high + low) / 2);
+            } else {
+                rowFound = true;
+            }
+        }
+        return mid;
+    }
+
+    let row  = 0;
+
+    if(matrix.length != 1) {
+        row = findRow(matrix,target);
+    }
+
+    //can implement binary search in case of very long row length
+    for(let i = 0; i < matrix[row].length; i++) {
+        if(matrix[row][i] === target) {
+            return true;
+        }
+    }
+
+    return false;
 };
+
+console.log(searchMatrix([[1],[3]],3));
+
+
+
+
 
 /*
 
--- leetcode/leetcode_playground.js
+++ leetcode/leetcode_playground.js
@@ -165,5 +165,7 @@
 // console.log(moveZeroes([0,0,0,1,2,3,0,0,0,4,5,0,0,0,0,0,0]));
 
 
-let arr = [1,2,3,4,7,11,12];
-console.log(arr[1] + arr[4]);
+let arr = [[1,2],[1,2],[1,2],[1,2],[1,2],[1,2],[1,2]];
+
+console.log(arr.length);
+
