9080f6f18c30902db617c6101cb38bdca147c067
patrickv77 <patrickv77@aol.com>
2022-05-23 15:12:25 -0500

    leetcode 1693 setup

---
+++ mysql_pract/leetcode/easy/1693_daily_leads_and_partners.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE 1693_daily_leads_and_partners;
+USE 1693_daily_leads_and_partners;
+
+Create table If Not Exists DailySales(date_id date, make_name varchar(20), lead_id int, partner_id int);
+Truncate table DailySales;
+insert into DailySales (date_id, make_name, lead_id, partner_id) values ('2020-12-8', 'toyota', '0', '1');
+insert into DailySales (date_id, make_name, lead_id, partner_id) values ('2020-12-8', 'toyota', '1', '0');
+insert into DailySales (date_id, make_name, lead_id, partner_id) values ('2020-12-8', 'toyota', '1', '2');
+insert into DailySales (date_id, make_name, lead_id, partner_id) values ('2020-12-7', 'toyota', '0', '2');
+insert into DailySales (date_id, make_name, lead_id, partner_id) values ('2020-12-7', 'toyota', '0', '1');
+insert into DailySales (date_id, make_name, lead_id, partner_id) values ('2020-12-8', 'honda', '1', '2');
+insert into DailySales (date_id, make_name, lead_id, partner_id) values ('2020-12-8', 'honda', '2', '1');
+insert into DailySales (date_id, make_name, lead_id, partner_id) values ('2020-12-7', 'honda', '0', '1');
+insert into DailySales (date_id, make_name, lead_id, partner_id) values ('2020-12-7', 'honda', '1', '2');
+insert into DailySales (date_id, make_name, lead_id, partner_id) values ('2020-12-7', 'honda', '2', '1');
+
+SELECT * FROM DailySales;
