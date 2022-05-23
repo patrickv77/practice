ba655c5f731f0edee391d34d677d28aebb466b88
patrickv77 <patrickv77@aol.com>
2022-05-23 15:15:29 -0500

    leetcode 1729 setup

---
+++ mysql_pract/leetcode/easy/1729_find_followers_count.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE 1729_find_followers_count;
+USE 1729_find_followers_count;
+
+Create table If Not Exists Followers(user_id int, follower_id int);
+Truncate table Followers;
+insert into Followers (user_id, follower_id) values ('0', '1');
+insert into Followers (user_id, follower_id) values ('1', '0');
+insert into Followers (user_id, follower_id) values ('2', '0');
+insert into Followers (user_id, follower_id) values ('2', '1');
+
+SELECT * FROM Followers;
