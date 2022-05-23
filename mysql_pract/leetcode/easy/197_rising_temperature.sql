56659d4e44791e52738a7712eb8ef5b828a4a9e0
patrickv77 <patrickv77@aol.com>
2022-05-23 14:23:23 -0500

    leetcode 197 solution

--- mysql_pract/leetcode/easy/197_rising_temperature.sql
+++ mysql_pract/leetcode/easy/197_rising_temperature.sql
@@ -9,3 +9,10 @@ insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20
 insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30');
 
 SELECT * FROM Weather;
+
+SELECT p.id FROM Weather p,Weather q where p.temperature > q.temperature AND DATEDIFF(p.recordDate,q.recordDate)=1;
+
+/*
+Runtime: 420 ms, faster than 76.91% of MySQL online submissions for Rising Temperature.
+Memory Usage: 0B, less than 100.00% of MySQL online submissions for Rising Temperature.
+*/
