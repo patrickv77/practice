da17272b41b3d36ebe3d885805590d39a6c3cb7b
patrickv77 <patrickv77@aol.com>
2022-05-23 15:18:01 -0500

    leetcode 1729 solution

--- mysql_pract/leetcode/easy/1729_find_followers_count.sql
+++ mysql_pract/leetcode/easy/1729_find_followers_count.sql
@@ -9,3 +9,13 @@ insert into Followers (user_id, follower_id) values ('2', '0');
 insert into Followers (user_id, follower_id) values ('2', '1');
 
 SELECT * FROM Followers;
+
+SELECT user_id, COUNT(DISTINCT(follower_id)) AS followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id;
+
+/*
+Runtime: 752 ms, faster than 14.97% of MySQL online submissions for Find Followers Count.
+Memory Usage: 0B, less than 100.00% of MySQL online submissions for Find Followers Count.
+*/
