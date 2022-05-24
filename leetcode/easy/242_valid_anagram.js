f2c553cdcd359216888f3adf2a86514c61584eee
patrickv77 <patrickv77@aol.com>
2022-05-24 16:13:54 -0500

    leetcode 242 solution

--- leetcode/easy/242_valid_anagram.js
+++ leetcode/easy/242_valid_anagram.js
@@ -10,9 +10,28 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
+    if(s.length != t.length) return false;
 
+    let res = new Array(26);
+    res.fill(0);
+
+    for(let i = 0; i < s.length; i++) {
+        res[s.charCodeAt(i)-97]++;
+        res[t.charCodeAt(i)-97]--;
+    }
+
+    for(let i = 0; i < res.length; i++) {
+        if(res[i] != 0) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
-/*
+console.log(isAnagram("rat","car"));
 
+/*
+Runtime: 69 ms, faster than 98.30% of JavaScript online submissions for Valid Anagram.
+Memory Usage: 42.4 MB, less than 95.38% of JavaScript online submissions for Valid Anagram.
 */
