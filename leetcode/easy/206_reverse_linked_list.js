b8fe14fc679304259ddd216d535980ab31b6d5e9
patrickv77 <patrickv77@aol.com>
2022-05-25 15:35:43 -0500

    leetcode 206 setup

---
+++ leetcode/easy/206_reverse_linked_list.js
@@ -0,0 +1,22 @@
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+*/
+
+/*
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/*
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    
+};
+
+/*
+
+ */
