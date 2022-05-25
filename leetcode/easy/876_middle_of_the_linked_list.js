1be5625fa97e2b908388772f06022fc2002b7db0
patrickv77 <patrickv77@aol.com>
2022-05-25 12:22:20 -0500

    leetcode 876 solution

--- leetcode/easy/876_middle_of_the_linked_list.js
+++ leetcode/easy/876_middle_of_the_linked_list.js
@@ -16,10 +16,24 @@ If there are two middle nodes, return the second middle node.
  * @return {ListNode}
  */
 var middleNode = function(head) {
-    
+    if(head === null) return null;
+    if(head.next === null) return head;
+
+    let res = head, countTwo = head;
+
+    while(countTwo.next != null && countTwo.next.next != null) {
+        res = res.next;
+        countTwo = countTwo.next.next;
+    }
+
+    if(countTwo.next != null) {
+        res = res.next;
+    }
+
+    return res;
 };
 
-
 /*
-
+Runtime: 67 ms, faster than 72.62% of JavaScript online submissions for Middle of the Linked List.
+Memory Usage: 42.3 MB, less than 33.70% of JavaScript online submissions for Middle of the Linked List.
 */
