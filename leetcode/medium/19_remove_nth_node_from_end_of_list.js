d9d6a038d8677e0ff0cfb48c1c7f3e99ad22ffc8
patrickv77 <patrickv77@aol.com>
2022-05-25 12:31:19 -0500

    leetcode 19 setup

---
+++ leetcode/medium/19_remove_nth_node_from_end_of_list.js
@@ -0,0 +1,23 @@
+/*
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+*/
+
+/*
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/*
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    
+};
+
+/*
+
+*/
