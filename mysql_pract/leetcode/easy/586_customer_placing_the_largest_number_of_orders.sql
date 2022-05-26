9a298073c75290caa16e7c6bfd18be74a1fb8976
patrickv77 <patrickv77@aol.com>
2022-05-26 11:57:29 -0500

    leetcode 586 solution

--- mysql_pract/leetcode/easy/586_customer_placing_the_largest_number_of_orders.sql
+++ mysql_pract/leetcode/easy/586_customer_placing_the_largest_number_of_orders.sql
@@ -10,6 +10,13 @@ insert into orders (order_number, customer_number) values ('4', '3');
 
 SELECT * FROM orders;
 
-/*
+SELECT customer_number FROM orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1;
 
+
+/*
+Runtime: 493 ms, faster than 47.26% of MySQL online submissions for Customer Placing the Largest Number of Orders.
+Memory Usage: 0B, less than 100.00% of MySQL online submissions for Customer Placing the Largest Number of Orders.
 */
