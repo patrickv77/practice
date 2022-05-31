eb7586584d2c086b20d4bd87b86789e8f867f7d0
patrickv77 <patrickv77@aol.com>
2022-05-31 14:35:48 -0500

    leetcode 771 solution

--- leetcode/easy/771_jewels_and_stones.js
+++ leetcode/easy/771_jewels_and_stones.js
@@ -10,9 +10,28 @@ Letters are case sensitive, so "a" is considered a different type of stone from 
  * @return {number}
  */
 var numJewelsInStones = function(jewels, stones) {
-    
+    let hm = new Map();
+    for(let i = 0; i < stones.length; i++) {
+        if(hm.has(stones.charAt(i))){
+            hm.set(stones.charAt(i),hm.get(stones.charAt(i))+1);
+        }else{
+            hm.set(stones.charAt(i),1);
+        }
+    }
+
+    let res = 0;
+    for(let i = 0; i < jewels.length; i++) {
+        if(hm.get(jewels.charAt(i))) {
+            res += hm.get(jewels.charAt(i));
+        }
+    }
+
+    return res;
 };
 
-/*
+console.log(numJewelsInStones("z", "ZZ"));
 
+/*
+Runtime: 93 ms, faster than 34.99% of JavaScript online submissions for Jewels and Stones.
+Memory Usage: 43.3 MB, less than 27.47% of JavaScript online submissions for Jewels and Stones.
 */
