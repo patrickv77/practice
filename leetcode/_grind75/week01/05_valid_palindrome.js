ca8205b1ead783e3e7c9dc2130b14b5f1d7aee80
patrickv77 <patrickv77@aol.com>
2022-06-01 15:31:48 -0500

    grind75 05 solution

--- leetcode/_grind75/week01/05_valid_palindrome.js
+++ leetcode/_grind75/week01/05_valid_palindrome.js
@@ -9,9 +9,22 @@ Given a string s, return true if it is a palindrome, or false otherwise.
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    var regex = /[^a-z0-9]/g;
 
+    s = s.toLowerCase();
+    s = s.replace(regex, "");
+
+    let left = 0, right = s.length-1; 
+    while(left < right) {
+        if(s.charAt(left) != s.charAt(right)) {
+            return false;
+        }
+        left++; right--;
+    }
+    return true;
 };
 
 /*
-
+Runtime: 105 ms, faster than 43.99% of JavaScript online submissions for Valid Palindrome.
+Memory Usage: 43.9 MB, less than 95.53% of JavaScript online submissions for Valid Palindrome.
 */
