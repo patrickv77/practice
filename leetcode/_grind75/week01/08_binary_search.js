487597ab3fc6f34f97e5ed6777fcb64e74906f0a
patrickv77 <patrickv77@aol.com>
2022-06-02 15:46:47 -0500

    grind75 08 solution

--- leetcode/_grind75/week01/08_binary_search.js
+++ leetcode/_grind75/week01/08_binary_search.js
@@ -10,9 +10,22 @@ You must write an algorithm with O(log n) runtime complexity.
  * @return {number}
  */
 var search = function(nums, target) {
-    
+    let len = nums.length;
+    let l = 0, r = len-1;
+    while(l <= r) {
+        let mid = Math.floor((l+r)/2);
+        if(nums[mid]===target){
+            return mid;
+        }else if(nums[mid] > target) {
+            r = mid-1;
+        }else{
+            l = mid+1;
+        }
+    }
+    return -1;
 };
 
 /*
-
+Runtime: 81 ms, faster than 58.70% of JavaScript online submissions for Binary Search.
+Memory Usage: 45.4 MB, less than 14.56% of JavaScript online submissions for Binary Search.
 */
