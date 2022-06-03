beb930014ddb1f1c7f09cfc9fabe61d47fb429d0
patrickv77 <patrickv77@aol.com>
2022-06-03 11:12:11 -0500

    grind75 12 setup

---
+++ leetcode/_grind75/week01/12_balanced_binary_tree.js
@@ -0,0 +1,27 @@
+/*
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+*/
+
+/*
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/*
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    
+};
+
+/*
+
+*/
