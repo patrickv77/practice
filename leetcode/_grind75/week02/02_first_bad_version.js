307658e1b808b1e179f8e5bf9025ec9401e952c0
patrickv77 <patrickv77@aol.com>
2022-06-03 13:21:40 -0500

    grind75 0202 solution

--- leetcode/_grind75/week02/02_first_bad_version.js
+++ leetcode/_grind75/week02/02_first_bad_version.js
@@ -26,10 +26,23 @@ You are given an API bool isBadVersion(version) which returns whether version is
      * @return {integer} The first bad version
      */
     return function(n) {
-        
+        let r = n;
+        let l = 0;
+        while(l <= r) {
+            let mid = Math.floor((l+r)/2);
+            let version = isBadVersion(mid);
+            if(version !== isBadVersion(mid+1)){
+                return mid+1;
+            }else if(version === false) {
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+        }
     };
 };
 
 /*
-
+Runtime: 101 ms, faster than 22.44% of JavaScript online submissions for First Bad Version.
+Memory Usage: 41.8 MB, less than 59.18% of JavaScript online submissions for First Bad Version.
 */
