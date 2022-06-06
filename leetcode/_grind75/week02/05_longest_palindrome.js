79fe53b33cdf1233fd690c4f406b4426cfc60e1d
patrickv77 <patrickv77@aol.com>
2022-06-06 12:46:42 -0500

    grind75 0205 solution

--- leetcode/_grind75/week02/05_longest_palindrome.js
+++ leetcode/_grind75/week02/05_longest_palindrome.js
@@ -9,9 +9,40 @@ Letters are case sensitive, for example, "Aa" is not considered a palindrome her
  * @return {number}
  */
 var longestPalindrome = function(s) {
-    
+    const hm = new Map();
+    let arr = [];
+
+    for(let i = 0; i < s.length; i++) {
+        let ch = s.charAt(i);
+        if(hm.get(ch)) {
+            hm.set(ch, hm.get(ch) + 1);
+        }else{
+            arr = [...arr, ch];
+            hm.set(ch, 1);
+        }
+    }
+
+    let res = 0;
+    let hasOdd = false;
+
+    for(let i = 0; i < arr.length; i++) {
+        let num = hm.get(arr[i]);
+        if(num%2 === 0) {
+            res += num;
+        }else{
+            res += num - 1;
+            hasOdd = true;
+        }
+    }
+
+    if(hasOdd){
+        res++;
+    }
+
+    return res;
 };
 
 /*
-
+Runtime: 106 ms, faster than 28.02% of JavaScript online submissions for Longest Palindrome.
+Memory Usage: 45.2 MB, less than 8.56% of JavaScript online submissions for Longest Palindrome.
 */
